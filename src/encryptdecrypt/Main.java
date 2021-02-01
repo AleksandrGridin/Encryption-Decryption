148d8a207423378f75683c574c19805e593ce69b
Aleksandr Gridin <aleksandr.n.gridin@gmail.com>
2021-02-01 15:50:36 +0300

    Initializing of project

---
+++ src/encryptdecrypt/Main.java
@@ -0,0 +1,9 @@
+package encryptdecrypt;
+
+public class Main {
+    public static void main(String[] args) {
+        long a = 11111;
+        long b = 1111111;
+        System.out.println(a * b);
+    }
+}
