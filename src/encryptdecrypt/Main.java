5a690e67064cbe9fbe4d33f6a9e0ab8fef3dfa69
Aleksandr Gridin <aleksandr.n.gridin@gmail.com>
2021-02-01 17:36:43 +0300

    Stage 3/6

--- src/encryptdecrypt/Main.java
+++ src/encryptdecrypt/Main.java
@@ -1,25 +1,36 @@
 package encryptdecrypt;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        final int firstLetter = 97;
-        final int lastLetter = 122;
-        String line = "we found a treasure!";
-        char[] message = line.toCharArray();
-        char[] encryptedMessage = new char[message.length];
+        Scanner sc = new Scanner(System.in);
+        String category = sc.nextLine();
+        String line = sc.nextLine();
+        int key = sc.nextInt();
 
-        for (int i = 0; i < message.length; i++) {
-            if (!Character.isLetter(message[i])) {
-                encryptedMessage[i] = message[i];
-                continue;
+        if (category.equals("enc")) {
+            char[] message = line.toCharArray();
+            char[] encryptedMessage = new char[message.length];
+            for (int i = 0; i < message.length; i++) {
+                int ch = message[i];
+                int inCh = ch + key;
+                encryptedMessage[i] = (char) inCh;
             }
-            int ch = message[i];
-            int inCh = lastLetter - ch + firstLetter;
-            encryptedMessage[i] = (char) inCh;
+            System.out.println(new String(encryptedMessage));
         }
-        System.out.println(new String(encryptedMessage));
 
+        if (category.equals("dec")) {
+            char[] message = line.toCharArray();
+            char[] encryptedMessage = new char[message.length];
+            for (int i = 0; i < message.length; i++) {
+                int ch = message[i];
+                int inCh = ch - key;
+                encryptedMessage[i] = (char) inCh;
+            }
+            System.out.println(new String(encryptedMessage));
+        }
 
     }
 }
