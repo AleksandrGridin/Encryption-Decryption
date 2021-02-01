d63a16320115aab23effcbc5e2ce64d16497b9ac
Aleksandr Gridin <aleksandr.n.gridin@gmail.com>
2021-02-01 16:37:26 +0300

    Stage 1/6

--- src/encryptdecrypt/Main.java
+++ src/encryptdecrypt/Main.java
@@ -1,9 +1,25 @@
 package encryptdecrypt;
 
+
 public class Main {
     public static void main(String[] args) {
-        long a = 11111;
-        long b = 1111111;
-        System.out.println(a * b);
+        final int firstLetter = 97;
+        final int lastLetter = 122;
+        String line = "we found a treasure!";
+        char[] message = line.toCharArray();
+        char[] encryptedMessage = new char[message.length];
+
+        for (int i = 0; i < message.length; i++) {
+            if (!Character.isLetter(message[i])) {
+                encryptedMessage[i] = message[i];
+                continue;
+            }
+            int ch = message[i];
+            int inCh = lastLetter - ch + firstLetter;
+            encryptedMessage[i] = (char) inCh;
+        }
+        System.out.println(new String(encryptedMessage));
+
+
     }
 }
